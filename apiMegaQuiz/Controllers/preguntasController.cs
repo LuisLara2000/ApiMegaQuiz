56154c8efea5f93fe7d6771ab9d97b94f7e12e3c
LUJAAA <javier.olivareslr@uanl.edu.mx>
2024-09-21 15:55:43 -0600

    Agregar archivos de proyecto.

---
+++ apiMegaQuiz/Controllers/preguntasController.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// importo las librerias para gestionar la base de datos
+using System.Data;
+using System.Data.SqlClient;
+// importo los modelos
+using apiMegaQuiz.Models;
+// AGREGAR CORDS
+
+namespace apiMegaQuiz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class preguntasController : Controller
+    {
+        // obtener la cadena de conexion a la base de datos
+        private readonly string cadenaSQL;
+        // contructo, obtiene la cadena de conexion 
+        public preguntasController(IConfiguration config)
+        {
+            cadenaSQL = config.GetConnectionString("CadenaSQL");
+        }
+
+        [HttpGet]
+        [Route("ListaPreguntas")]
+        public IActionResult ListaPreguntas()
+        {
+            List<preguntas> lista = new List<preguntas>();
+
+            try
+            {
+                // me conecto a la base de datos
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {
+                    // abro la conexion
+                    conexion.Open();
+                    // asigno el procedimiento almacenado
+                    var cmd = new SqlCommand("sp_mostrarTodasPreguntas", conexion);
+                    // defino el tipo de comando
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    // con los datos que devuelve el procedimieto almacenado
+                    using (var rd = cmd.ExecuteReader())
+                    {
+                        // leo los datos
+                        while(rd.Read())
+                        {
+                            // cada dato lo asigno a un nuevo elemento en la lista de preguntas
+                            lista.Add(new preguntas
+                            {
+                                idPregunta = Convert.ToInt32(rd["idPregunta"]),
+                                pregunta = rd["pregunta"].ToString(),
+                                estado = Convert.ToInt32(rd["estado"]),
+                            });
+
+                        }
+                    }
+                }
+                // retono los datos
+                return StatusCode(StatusCodes.Status200OK, new { mesnaje = "Lista de pregunta devuelta :)", Response = lista });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mesnaje = "ERROR :(", Response = ex.Message });
+            }
+        }
+
+        // ESTE PROCEDIMETO ALMACENADO DEBE RETORNAR LA LISTA FILTRABA, Y NO FILTRARLA AQUI, SEGUN YO XD
+        [HttpGet]
+        [Route("ListaPreguntasEspecificas/{estado:int}")]
+        public IActionResult ListaPreguntasEspecificas(int estado)
+        {
+            List<preguntas> lista = new List<preguntas>();
+
+            try
+            {
+                // me conecto a la base de datos
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {
+                    // abro la conexion
+                    conexion.Open();
+                    // asigno el procedimiento almacenado
+                    var cmd = new SqlCommand("sp_mostrarTodasPreguntas", conexion);
+                    // defino el tipo de comando
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    // con los datos que devuelve el procedimieto almacenado
+                    using (var rd = cmd.ExecuteReader())
+                    {
+                        // leo los datos
+                        while (rd.Read())
+                        {
+                            // valido que la pregunta sea del valor seleccionado
+                            if (Convert.ToInt32(rd["estado"]) == estado){
+                                // cada dato lo asigno a un nuevo elemento en la lista de preguntas
+                                lista.Add(new preguntas
+                                {
+                                    idPregunta = Convert.ToInt32(rd["idPregunta"]),
+                                    pregunta = rd["pregunta"].ToString(),
+                                    estado = Convert.ToInt32(rd["estado"]),
+                                });
+                            }
+                        }
+                    }
+                }
+                // retono los datos
+                return StatusCode(StatusCodes.Status200OK, new { mesnaje = "Lista de pregunta especifica devuelta :)", Response = lista });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mesnaje = "ERROR :(", Response = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        [Route("ObtenerPregunta/{idPregunta:int}")]
+        public IActionResult ObtenerPregunta(int idPregunta)
+        {
+            preguntas oPreguntas = new preguntas();
+
+            try
+            {
+                // me conecto a la base de datos
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {
+                    // abro la conexion
+                    conexion.Open();
+                    // asigno el procedimiento almacenado
+                    var cmd = new SqlCommand("sp_mostrarUnaPregunta", conexion);
+                    // le paso el id de la pregunta que quiero
+                    cmd.Parameters.AddWithValue("@idPregunta",idPregunta);  
+                    // defino el tipo de comando
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    // con los datos que devuelve el procedimieto almacenado
+                    using (var rd = cmd.ExecuteReader())
+                    {
+                        // leo los datos
+                        while (rd.Read())
+                        {
+                            // cada dato lo ingreso al objeto pregunta
+                            oPreguntas.pregunta = rd["pregunta"].ToString();
+                            oPreguntas.rCorrecta= rd["rCorrecta"].ToString();
+                            oPreguntas.rIncorrecta1 = rd["rIncorrecta1"].ToString();
+                            oPreguntas.rIncorrecta2 = rd["rIncorrecta2"].ToString();
+                            oPreguntas.rIncorrecta3 = rd["rIncorrecta3"].ToString();
+                            oPreguntas.rIncorrecta4 = rd["rIncorrecta4"].ToString();
+                            oPreguntas.rIncorrecta5 = rd["rIncorrecta5"].ToString();                  
+                        }
+                    }
+                }
+                // retono los datos
+                return StatusCode(StatusCodes.Status200OK, new { mesnaje = "Pregunta devuelta :)", Response = oPreguntas });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mesnaje = "ERROR :(", Response = ex.Message });
+            }
+        }
+    }
+}
---
+++ apiMegaQuiz/Models/preguntas.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace apiMegaQuiz.Models
+{
+    public class preguntas
+    {
+        public int idPregunta {  get; set; }
+        public string pregunta { get; set; }
+        public string rCorrecta { get; set; }
+        public string rIncorrecta1 { get; set; }
+        public string rIncorrecta2 { get; set; }
+        public string rIncorrecta3 { get; set; }
+        public string rIncorrecta4 { get; set; }
+        public string rIncorrecta5 { get; set; }
+        public string rIncorrecta6 { get; set; }
+        public int estado { get; set; }
+        public int dificultad {  get; set; }
+
+
+    }
+}
